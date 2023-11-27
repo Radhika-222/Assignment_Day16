f3c629783b4aaab5af222df90b829ffd239bf37a
Radhika-222 <wandareradhika@gmail.com>
2023-11-27 16:00:52 +0530

    initial commit

---
+++ AssignmentPart1_Day16/AssignmentPart1_Day16/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AssignmentPart1_Day16
+{
+    public delegate int ArithmeticOperation(int a, int b);
+    public class Program
+    {
+        static int Add(int a, int b)
+        {
+           int result = a + b;
+            return result;
+        }
+        static int Subtract(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+        static int Multiply(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+        static int Divide(int a, int b)
+        {
+            int result = a / b;
+            return result;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            ArithmeticOperation addDel = new ArithmeticOperation(Add);
+            ArithmeticOperation subtractDel = new ArithmeticOperation(Subtract);
+            ArithmeticOperation multiplyDel = new ArithmeticOperation(Multiply);
+            ArithmeticOperation divideDel = new ArithmeticOperation(Divide);
+
+            Console.WriteLine("Enter first integer");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second integer");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose an arithmetic operation:");
+            Console.WriteLine("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = addDel(num1, num2);
+                    break;
+                case 2:
+                    result = subtractDel(num1, num2);
+                    break;
+                case 3:
+                    result = multiplyDel(num1, num2);
+                    break;
+                case 4:
+                    result = divideDel(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {result}");
+            Console.ReadLine();
+        }
+    }
+
+}
+    
---
+++ Assignment_Part2/Assignment_Part2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Assignment_Part2
+{
+    public delegate T Operation<T>(T a, T b);
+    internal class Program
+    {
+        static T Add<T>(T a, T b) => (dynamic)a + b;
+        static T Subtract<T>(T a, T b) => (dynamic)a - b;
+        static T Multiply<T>(T a, T b) => (dynamic)a * b;
+        static T Divide<T>(T a, T b) => (dynamic)a/ b;
+        static void Main(string[] args)
+        {
+            Operation<double> Del1 = new Operation<double>(Add);
+            Operation<double> Del2 = new Operation<double>(Subtract);
+            Operation<double> Del3 = new Operation<double>(Multiply);
+            Operation<double> Del4 = new Operation<double>(Divide);
+            Console.WriteLine("Enter First Number");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Choose which operation you want to perform on the Above numbers. \n1.Addition \n2.Subractionc\n3.Multiplication. \n4.Division");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine($"Addition of {num1} and {num2} is " + Del1(num1, num2));
+                    break;
+                case 2:
+                    Console.WriteLine($"Subraction of {num1} and {num2} is " + Del2(num1, num2));
+                    break;
+                case 3:
+                    Console.WriteLine($"Multiplication of {num1} and {num2} is " + Del3(num1, num2));
+                    break;
+                case 4:
+                    Console.WriteLine($"Division of {num1} and {num2} is " + Del4(num1, num2));
+                    break;
+                default:
+                    Console.WriteLine("You chosse invalid operation !!!");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
